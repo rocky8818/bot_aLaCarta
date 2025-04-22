2c70e5224238a811d59ef247c2c45249defc086c
rocky8818 <rickyglz.1318@gmail.com>
2025-04-22 11:43:17 -0600

    🐛 fix regex bug on get blacklist

--- src/flows/GetBlacklist.ts
+++ src/flows/GetBlacklist.ts
@@ -2,16 +2,17 @@
 import { addKeyword } from "@builderbot/bot";
 import { whitelist, blacklist } from "../lib/shopFunctions";
 
-const GetBlacklist = addKeyword(["Get Blacklist"], {
-  regex: true,
-})
-  .addAction(async (ctx, { endFlow, state, flowDynamic }) => {
+const GetBlacklist = addKeyword(["Get Blacklist"])
+
+  .addAction(async (ctx, { endFlow }) => {
     // Verificar si el usuario que intenta desbloquear está en la whitelist
     if (!whitelist.includes(ctx.from)) {
       return endFlow("No tienes permiso para realizar esta acción.");
     }
+  })
 
-    await flowDynamic(blacklist.join('_--_'))
+  .addAction(async (ctx, { flowDynamic }) => {
+    await flowDynamic('-_-'+blacklist.join('-_-')+'-_-')
   })
 
 export default GetBlacklist;
